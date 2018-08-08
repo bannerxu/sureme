46e85e7f4fe6302d757255cb47b2abde42090ceb
jinguoguo <zwj_995@126.com>
2018-08-08 20:05:06 +0800

    cms update 卡券模块

--- service/src/main/java/top/xuguoliang/service/coupon/cms/CouponCmsResultVO.java
+++ service/src/main/java/top/xuguoliang/service/coupon/cms/CouponCmsResultVO.java
@@ -19,11 +19,20 @@ public class CouponCmsResultVO {
     @ApiModelProperty("卡券名")
     private String couponName;
 
-    @ApiModelProperty("对应折扣")
-    private BigDecimal couponDiscount;
-
     @ApiModelProperty("关联的商品")
     private List<Commodity> commodities;
+
+    @ApiModelProperty("领取开始时间")
+    private Date pullBeginTime;
+
+    @ApiModelProperty("领取结束时间")
+    private Date pullEndTime;
+
+    @ApiModelProperty("使用开始时间")
+    private Date useBeginTime;
+
+    @ApiModelProperty("使用结束时间")
+    private Date useEndTime;
 
     @ApiModelProperty("创建时间")
     private Date createTime;
--- service/src/main/java/top/xuguoliang/service/coupon/cms/CouponCmsUpdateVO.java
+++ service/src/main/java/top/xuguoliang/service/coupon/cms/CouponCmsUpdateVO.java
@@ -6,6 +6,7 @@ import org.hibernate.validator.constraints.NotBlank;
 
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,11 +23,20 @@ public class CouponCmsUpdateVO {
     @ApiModelProperty("卡券名")
     private String couponName;
 
-    @NotNull(message = "折扣不能为空")
-    @ApiModelProperty("对应折扣")
-    private BigDecimal couponDiscount;
-
     @ApiModelProperty("数组：商品id")
     private List<Integer> commodityIds;
+
+    @ApiModelProperty("领取开始时间")
+    private Date pullBeginTime;
+
+    @ApiModelProperty("领取结束时间")
+    private Date pullEndTime;
+
+    @ApiModelProperty("使用开始时间")
+    private Date useBeginTime;
+
+    @ApiModelProperty("使用结束时间")
+    private Date useEndTime;
+
 
 }
