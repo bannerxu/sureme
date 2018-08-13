6e36a4a13e457fce144d5b7dfe6bef5f8a1b0faa
jinguoguo <zwj_995@126.com>
2018-08-14 03:49:33 +0800

    web update 文章评论

--- service/src/main/java/top/xuguoliang/service/article/ArticleWebService.java
+++ service/src/main/java/top/xuguoliang/service/article/ArticleWebService.java
@@ -278,6 +278,7 @@ public class ArticleWebService {
             articleComment = new ArticleComment();
             articleComment.setCommentContent(commentContent);
             articleComment.setCreateTime(new Date());
+            articleComment.setArticleId(articleId);
             articleCommentDao.saveAndFlush(articleComment);
         } else {
             throw new ValidationException(MessageCodes.WEB_USER_COMMENT_EXIST);
