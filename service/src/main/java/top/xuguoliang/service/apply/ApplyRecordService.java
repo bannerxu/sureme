e8fbfdd1aa3dd219705bd10011b93fde36e4e938
jinguoguo <zwj_995@126.com>
2018-08-24 10:18:28 +0800

    web add 订单模块 添加退款申请； cms add 审核模块 添加查询和审核方法；

--- service/src/main/java/top/xuguoliang/service/apply/ApplyRecordService.java
+++ service/src/main/java/top/xuguoliang/service/apply/ApplyRecordService.java
@@ -1,20 +1,32 @@
 package top.xuguoliang.service.apply;
 
+import com.github.binarywang.wxpay.bean.request.WxPayRefundRequest;
+import com.github.binarywang.wxpay.bean.result.WxPayRefundResult;
+import com.github.binarywang.wxpay.exception.WxPayException;
+import com.github.binarywang.wxpay.service.impl.WxPayServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import top.xuguoliang.common.exception.MessageCodes;
 import top.xuguoliang.common.exception.ValidationException;
 import top.xuguoliang.common.utils.BeanUtils;
 import top.xuguoliang.models.apply.ApplyRecord;
 import top.xuguoliang.models.apply.ApplyRecordDao;
 import top.xuguoliang.models.apply.ApplyStatus;
+import top.xuguoliang.models.moneywater.MoneyWater;
+import top.xuguoliang.models.moneywater.MoneyWaterDao;
+import top.xuguoliang.models.moneywater.MoneyWaterType;
+import top.xuguoliang.models.order.Order;
+import top.xuguoliang.models.order.OrderDao;
+import top.xuguoliang.models.order.OrderStatusEnum;
 import top.xuguoliang.service.apply.cms.ApplyRecordVO;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.Date;
 
 /**
@@ -27,6 +39,12 @@ public class ApplyRecordService {
 
     @Resource
     private ApplyRecordDao applyRecordDao;
+
+    @Resource
+    private OrderDao orderDao;
+
+    @Resource
+    private MoneyWaterDao moneyWaterDao;
 
     /**
      * 分页查询申请记录
@@ -44,8 +62,9 @@ public class ApplyRecordService {
 
     /**
      * 审核
+     *
      * @param applyRecordId 申请记录id
-     * @param isPass 是否通过
+     * @param isPass        是否通过
      */
     public void audit(Integer applyRecordId, Integer isPass) {
         Date now = new Date();
@@ -59,8 +78,61 @@ public class ApplyRecordService {
         } else {
             applyRecord.setApplyStatus(ApplyStatus.APPLY_SUCCESS);
             // TODO 退款申请通过，发起微信退款；另外需要在退款回调中设置订单状态和添加流水记录
+            Integer orderId = applyRecord.getOrderId();
+            Order order = orderDao.findOne(orderId);
+            if (ObjectUtils.isEmpty(order) || order.getDeleted()) {
+                logger.error("退款失败：订单{} 不存在", orderId);
+                throw new ValidationException(MessageCodes.CMS_ORDER_NOT_EXIST);
+            }
+            String orderNumber = order.getOrderNumber();
+            try {
+                WxPayRefundResult refundResult = refund(orderNumber, order.getRealPayMoney());
+                String returnCode = refundResult.getReturnCode();
+                if (StringUtils.isEmpty(returnCode)) {
+                    logger.error("申请退款失败：调用微信申请退款接口返回值为空");
+                }
+                if ("SUCCESS".equals(returnCode)) {
+                    logger.info("订单id{}，订单号{} 退款成功", orderId, orderNumber);
+                    // 退款成功
+                    order.setOrderStatus(OrderStatusEnum.ORDER_REFUNDED);
+                    orderDao.save(order);
+                    MoneyWater moneyWater = new MoneyWater();
+                    moneyWater.setUserId(order.getUserId());
+                    moneyWater.setType(MoneyWaterType.REFUND);
+                    moneyWater.setMoney(order.getRealPayMoney());
+                    moneyWater.setTime(now);
+                    moneyWater.setOrderId(orderId);
+                    moneyWater.setDeleted(false);
+                    moneyWaterDao.save(moneyWater);
+                } else {
+                    logger.info("订单id{}，订单号{} 退款失败：{}", orderId, orderNumber, refundResult.getReturnMsg());
+                }
+
+            } catch (WxPayException e) {
+                e.printStackTrace();
+            }
         }
         applyRecord.setAuditTime(now);
         applyRecordDao.save(applyRecord);
+    }
+
+    /**
+     * 发起微信退款申请
+     *
+     * @param orderNumber  订单号码
+     * @param realPayMoney 退款金额
+     * @return 退款申请结果
+     * @throws WxPayException
+     */
+    private WxPayRefundResult refund(String orderNumber, BigDecimal realPayMoney) throws WxPayException {
+        WxPayRefundRequest wxPayRefundRequest = new WxPayRefundRequest();
+//        wxPayRefundRequest.setNotifyUrl("https://");
+        wxPayRefundRequest.setOutTradeNo(orderNumber);
+        int money = realPayMoney.multiply(new BigDecimal("100")).intValue();
+        wxPayRefundRequest.setTotalFee(money);
+        wxPayRefundRequest.setRefundFee(money);
+        wxPayRefundRequest.setOutRefundNo(orderNumber);
+
+        return new WxPayServiceImpl().refund(wxPayRefundRequest);
     }
 }
