2730e55af5991480f88b1d28f31de66a8bbaf6a4
banner <1935599727@qq.com>
2019-03-25 00:55:25 +0800

    添加kotlin 编辑器插件 https://www.kotlincn.net/docs/reference/compiler-plugins.html

--- web/src/test/kotlin/top/banner/TestA.kt
+++ web/src/test/kotlin/top/banner/TestA.kt
@@ -1,16 +1,17 @@
 package top.banner
 
 import org.junit.Test
-import org.springframework.test.web.servlet.MockMvc
+import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
-import org.junit.runner.RunWith
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultHandler
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import top.banner.common.exception.ValidationException
 
 
 /**
@@ -28,8 +29,12 @@ class TestA {
     @Test
     fun exampleTest() {
         this.mockMvc.perform(get("/")).andExpect(status().is2xxSuccessful)
-                .andExpect(content().string("Hello"))
+                .andDo(print()).andExpect(content().string("Hello"))
 
     }
 
+
+    fun print(): ResultHandler {
+        return MockMvcResultHandlers.print()
+    }
 }
